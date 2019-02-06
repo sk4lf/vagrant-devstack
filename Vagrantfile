7a7d5e6808a31f00b1603479a7eb3be621f39736
Oleksii Kolodiazhnyi <akolodyazhnyy@mirantis.com>
2019-02-06 16:12:48 +0200

    added resources and removed swap. disabled plugins

--- Vagrantfile
+++ Vagrantfile
@@ -14,13 +14,17 @@ end
 Vagrant.configure("2") do |config|
   config.vm.provision "shell", inline: "echo \"#{hosts}\" >> /etc/hosts"
   config.vm.provider "virtualbox" do |vb|
-    vb.memory = "4096"
-    vb.cpus = "2"
+    vb.memory = "8192"
+    vb.cpus = "4"
+#    vb.disksize = '20GB'
+#    vb.vm.provision "shell", inline: <<-SHELL
+#        sudo resize2fs /dev/sda1
+#    SHELL
   end
   nodes.each do |node|
     config.vm.define "#{node['name']}" do |inst|
       inst.vm.box = node['box']
-      #inst.vm.hostname = node['name']
+      inst.vm.hostname = node['name']
       config.vm.provision :shell, inline: "hostnamectl set-hostname #{node['name']}"
       inst.vm.network "private_network", ip: node['ip']
       inst.vm.provision "shell", path: "provision/#{node['bootstrap']}", args: "#{node['name']}"
--- provision/devstack.sh
+++ provision/devstack.sh
@@ -5,6 +5,7 @@
 #--------------------------------------------------------------
 ADMIN_PASSWORD=secret
 HOST_IP=`ifconfig enp0s8 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`
+BRANCH='master'
 #--------------------------------------------------------------
 
 #--------------------------------------------------------------
@@ -18,27 +19,27 @@ sudo apt-get update
 #--------------------------------------------------------------
 # size of swapfile in megabytes
 
-swapsize=4000
+#swapsize=4000
 
 # does the swap file already exist?
-grep -q "swapfile" /etc/fstab
+#grep -q "swapfile" /etc/fstab
 
 # if not then create it
-if [ $? -ne 0 ]; then
-  echo 'swapfile not found. Adding swapfile.'
-  fallocate -l ${swapsize}M /swapfile
-  chmod 600 /swapfile
-  mkswap /swapfile
-  swapon /swapfile
-  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
-else
-  echo 'swapfile found. No changes made.'
-fi
+#if [ $? -ne 0 ]; then
+#  echo 'swapfile not found. Adding swapfile.'
+#  fallocate -l ${swapsize}M /swapfile
+#  chmod 600 /swapfile
+#  mkswap /swapfile
+#  swapon /swapfile
+#  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
+#else
+#  echo 'swapfile found. No changes made.'
+#fi
 
 # output results to terminal
-df -h
-cat /proc/swaps
-cat /proc/meminfo | grep Swap
+#df -h
+#cat /proc/swaps
+#cat /proc/meminfo | grep Swap
 #--------------------------------------------------------------
 
 
@@ -49,10 +50,20 @@ sudo useradd -s /bin/bash -d /opt/stack -m stack
 
 echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack
 
-sudo -u stack git clone https://git.openstack.org/openstack-dev/devstack /opt/stack/devstack
+sudo -u stack git clone -b $BRANCH https://git.openstack.org/openstack-dev/devstack /opt/stack/devstack
 
 sudo -u stack cat << EOF > /opt/stack/devstack/local.conf
 [[local|localrc]]
+#
+# Enabled plugins
+#-----------------
+#enable_plugin congress https://git.openstack.org/openstack/congress
+#enable_plugin monasca https://git.openstack.org/openstack/monasca-api
+#enable_plugin contrail https://github.com/zioc/contrail-devstack-plugin.git
+enable_plugin trove git://git.openstack.org/openstack/trove
+#
+# Services configuration
+#------------------------
 #FLAT_INTERFACE=enp0s3
 HOST_IP=$HOST_IP
 ADMIN_PASSWORD=$ADMIN_PASSWORD
